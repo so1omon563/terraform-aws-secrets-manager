b8c8e76cc0fdb3c8dfc4adee0d4b2fc519d3ffb9
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-06-27 07:44:17 -0700

    Initial commit

---
+++ scripts/rmtf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Get the root directory of the repo
+repo_root=$(git rev-parse --show-toplevel)
+
+# Get rid of any local caches
+find ${repo_root} -name ".terraform" -type d -exec rm -rf "{}" \; &> /dev/null
+find ${repo_root} -name ".terraform.lock.hcl" -type f -exec rm -rf "{}" \; &> /dev/null
